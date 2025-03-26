8fa5ae07791eef48983d47212b13d092642d1076
BobBieNeno <bie007x@gmail.com>
2025-03-26 13:22:48 +0700

    connect db

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
-func main (){
-	
+import (
+	"fmt"
+	"go-final/model"
+
+	"github.com/spf13/viper"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func main() {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(viper.Get("mysql.dsn"))
+	dsn := viper.GetString("mysql.dsn")
+
+	dialactor := mysql.Open(dsn)
+	db, err := gorm.Open(dialactor)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connection successful")
+
+	customer := []model.CartItem{}
+	result := db.Preload("ProductData").Preload("CartData").Find(&customer)
+	if result.Error != nil {
+		panic(result.Error)
+	}
+	fmt.Println(customer)
 }
