16715adc960bec44c6820973ef8172ad08d091ff
BobBieNeno <bie007x@gmail.com>
2025-03-26 14:05:26 +0700

    air success

--- main.go
+++ main.go
@@ -1,35 +1,34 @@
 package main
 
 import (
-	"fmt"
-	"go-final/model"
-
-	"github.com/spf13/viper"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"go-final/controller"
 )
 
 func main() {
-	viper.SetConfigName("config")
-	viper.AddConfigPath(".")
-	err := viper.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(viper.Get("mysql.dsn"))
-	dsn := viper.GetString("mysql.dsn")
+	// viper.SetConfigName("config")
+	// viper.AddConfigPath(".")
+	// err := viper.ReadInConfig()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(viper.Get("mysql.dsn"))
+	// dsn := viper.GetString("mysql.dsn")
 
-	dialactor := mysql.Open(dsn)
-	db, err := gorm.Open(dialactor)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Connection successful")
+	// dialactor := mysql.Open(dsn)
+	// db, err := gorm.Open(dialactor)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Connection successful")
 
-	customer := []model.Cart{}
-	result := db.Preload("CustomerData").Find(&customer)
-	if result.Error != nil {
-		panic(result.Error)
-	}
-	fmt.Println(customer)
+	// customer := []model.Cart{}
+	// result := db.Preload("CustomerData").Find(&customer)
+	// if result.Error != nil {
+	// 	panic(result.Error)
+	// }
+	// fmt.Println(customer)
+
+	// Release mode
+	//gin.SetMode(gin.ReleaseMode)
+	controller.StartServer()
 }
